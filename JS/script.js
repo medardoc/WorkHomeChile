d249a4390844d2f070c3b2fd35048e6ce54f6128
Medardo Carpio <166721587+medardoc@users.noreply.github.com>
2024-08-24 04:21:53 -0400

    Versionamientoi final de la pagina

--- JS/script.js
+++ JS/script.js
@@ -12,6 +12,28 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+let currentVideoIndex = 0;
+
+function showVideoSlides(index) {
+    const slides = document.querySelectorAll(".video-slide");
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) {
+        currentVideoIndex = 0;
+    } else if (index < 0) {
+        currentVideoIndex = totalSlides - 1;
+    } else {
+        currentVideoIndex = index;
+    }
+
+    document.querySelector('.video-slider-wrapper').style.transform = `translateX(-${currentVideoIndex * 100}%)`;
+}
+
+function plusVideoSlides(step) {
+    showVideoSlides(currentVideoIndex + step);
+}
+
+showVideoSlides(currentVideoIndex);
 
 
 // JavaScript para controlar el slider
@@ -31,104 +53,3 @@ function showSlides() {
     setTimeout(showSlides, 3000); // Cambiar imagen cada 3 segundos
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    const chatBubble = document.getElementById('chat-bubble');
-    const chatContainer = document.getElementById('chat-container');
-    const sendButton = document.getElementById('send-button');
-    const chatDisplay = document.getElementById('chat-display');
-    const userInput = document.getElementById('user-input');
-
-    chatBubble.addEventListener('click', function() {
-        chatContainer.style.display = chatContainer.style.display === 'none' ? 'block' : 'none';
-    });
-
-    sendButton.addEventListener('click', function() {
-        const userMessage = userInput.value;
-        appendMessage('user', userMessage);
-
-        if (isNaN(userMessage)) {
-            switch (userMessage.toLowerCase()) {
-                case 'whatsapp':
-                    setTimeout(function() {
-                        appendMessage('bot', '¡Claro! Puedes contactarnos a través de WhatsApp al número +123456789.');
-                    }, 500);
-                    break;
-                case 'llámanos':
-                    setTimeout(function() {
-                        appendMessage('bot', 'Si prefieres llamarnos, nuestro número de teléfono es +987654321. Estamos disponibles para ayudarte.');
-                    }, 500);
-                    break;
-                case 'correo':
-                    setTimeout(function() {
-                        appendMessage('bot', 'Si prefieres comunicarte por correo electrónico, puedes escribirnos a info@workhomechile.com. Te responderemos lo antes posible.');
-                    }, 500);
-                    break;
-                default:
-                    setTimeout(function() {
-                        appendMessage('bot', 
-                            '¡Bienvenidos a WorkHomeChile!' +
-                            '¿En qué puedo ayudarte?'+ 
-                            'Presiona 1: Envianos un WhatsApp, Presiona 2: Envianos un correo, Presiona 3: Llamanos.');
-                        }, 500);
-            }
-        } else {
-            const option = parseInt(userMessage);
-            switch (option) {
-                case 1:
-                    setTimeout(function() {
-                        appendMessage('bot', '¡Claro! Puedes contactarnos a través de WhatsApp al número +123456789.');
-                    }, 500);
-                    break;
-                case 2:
-                    setTimeout(function() {
-                        appendMessage('bot', 'Si prefieres comunicarte por correo electrónico, puedes escribirnos a info@workhomechile.com. Te responderemos lo antes posible.');
-                    }, 500);
-                    break;
-                case 3:
-                    setTimeout(function() {
-                        appendMessage('bot', 'Si prefieres llamarnos, nuestro número de teléfono es +987654321. Estamos disponibles para ayudarte.');
-                    }, 500);
-                    break;
-                default:
-                    setTimeout(function() {
-                        appendMessage('bot', 'No comprendo tu mensaje. ¿Puedes ser más específico?');
-                    }, 500);
-            }
-        }
-
-        userInput.value = ''; // Limpiar el campo de entrada después de enviar el mensaje
-    });
-
-    function appendMessage(sender, message) {
-        const messageContainer = document.createElement('div');
-        messageContainer.classList.add('chat-message');
-        messageContainer.innerHTML = `<div class="${sender}-message">${message}</div>`;
-        chatDisplay.appendChild(messageContainer);
-        chatDisplay.scrollTop = chatDisplay.scrollHeight; // Desplazar hacia abajo para mostrar el último mensaje
-    }
-});
-
-// Inicializa EmailJS con tu API Key
-emailjs.init('O5fNT_7RKdegz9ZLg');
-
-// Función para enviar el correo cuando el formulario es enviado
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevenir el envío normal del formulario
-
-    // Parámetros de la plantilla de correo
-    var templateParams = {
-        from_name: document.getElementById('name').value,
-        from_email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    };
-
-    // Enviar el correo electrónico
-    emailjs.send('service_rpccx7g', 'template_tyh7h41', templateParams)
-        .then(function(response) {
-           alert('Correo enviado con éxito!');
-           console.log('Correo enviado con éxito!', response.status, response.text);
-        }, function(error) {
-           alert('Error al enviar el correo.');
-           console.error('Error al enviar el correo:', error);
-        });
-});
